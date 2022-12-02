6ccc77058457d52c4953fb46a3d352cd632d258f
Стегнин Александр Александрович (stegnin_aa) <stegnin_aa@nlmk.com>
2022-12-02 21:13:45 +0500

    Обновлена конфигурация для запуска приложения

--- src/main/java/com/stegnin/jira/ved/JiraVedApplication.java
+++ src/main/java/com/stegnin/jira/ved/JiraVedApplication.java
@@ -1,9 +1,12 @@
 package com.stegnin.jira.ved;
 
+import com.stegnin.jira.ved.config.BotConfigurator;
 import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @EnableScheduling
 @SpringBootApplication
@@ -11,7 +14,10 @@ public class JiraVedApplication {
 
   @SneakyThrows
   public static void main(String[] args) {
-    SpringApplication.run(JiraVedApplication.class, args);
+    var context = SpringApplication.run(JiraVedApplication.class, args);
+    var bot = BotConfigurator.configure(context);
+    var api = new TelegramBotsApi(DefaultBotSession.class);
+    api.registerBot(bot);
   }
 
 }
