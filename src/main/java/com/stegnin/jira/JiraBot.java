3020cdffe15241ae0b757a6aa7b8ae73ec4908c3
Стегнин Александр Александрович (stegnin_aa) <stegnin_aa@nlmk.com>
2022-12-02 21:13:01 +0500

    Добавлен класс для бота

---
+++ src/main/java/com/stegnin/jira/JiraBot.java
@@ -0,0 +1,43 @@
+package com.stegnin.jira;
+
+import com.stegnin.jira.ved.config.property.TelegramBotProperty;
+import com.stegnin.jira.handler.UpdateReceiver;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+/**
+ * @author Alexandr Stegnin
+ */
+@Component
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class JiraBot extends TelegramLongPollingBot {
+
+  TelegramBotProperty telegramBotProperty;
+  UpdateReceiver updateReceiver;
+
+  @Override
+  public String getBotUsername() {
+    return telegramBotProperty.getName();
+  }
+
+  @Override
+  public String getBotToken() {
+    return telegramBotProperty.getToken();
+  }
+
+  @Override
+  @SneakyThrows
+  public void onUpdateReceived(Update update) {
+    var method = updateReceiver.handleUpdate(update);
+    execute(method);
+  }
+
+}
