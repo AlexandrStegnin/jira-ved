77b7c9dc3c721154959960e211a9b46f2559680c
Стегнин Александр Александрович (stegnin_aa) <stegnin_aa@nlmk.com>
2022-12-02 21:08:55 +0500

    Добавлен класс для получения конфигурационных данных бота.

---
+++ src/main/java/com/stegnin/jira/ved/config/property/TelegramBotProperty.java
@@ -0,0 +1,25 @@
+package com.stegnin.jira.ved.config.property;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * @author Alexandr Stegnin
+ */
+@Configuration
+@ConfigurationProperties(prefix = "telegram-bot")
+@Data
+@Validated
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TelegramBotProperty {
+  @NotBlank
+  String name;
+  @NotBlank
+  String token;
+}
