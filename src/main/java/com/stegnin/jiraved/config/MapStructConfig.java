c96a07e2ae467293736db544913a0a9259525486
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2022-12-05 13:42:36 +0500

    Добавлен конфиг для Map Struct

---
+++ src/main/java/com/stegnin/jiraved/config/MapStructConfig.java
@@ -0,0 +1,19 @@
+package com.stegnin.jiraved.config;
+
+import org.mapstruct.Builder;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
+
+/**
+ * @author Alexandr Stegnin
+ */
+@MapperConfig(
+    unmappedTargetPolicy = ReportingPolicy.IGNORE,
+    componentModel = "spring",
+    injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+    builder = @Builder(disableBuilder = true)
+)
+public interface MapStructConfig {
+}
+
