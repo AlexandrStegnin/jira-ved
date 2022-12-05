479b238bed30ada02af6fa797d1bebe766165b5b
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2022-12-05 16:34:11 +0500

    Добавлена заготовка для формирования сообщений пользователю

---
+++ src/main/java/com/stegnin/jiraved/service/BotMessageBuilder.java
@@ -0,0 +1,40 @@
+package com.stegnin.jiraved.service;
+
+import com.stegnin.jiraved.utils.MessageUtils;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+/**
+ * @author Alexandr Stegnin
+ */
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class BotMessageBuilder {
+
+    BotButtonManager botButtonManager;
+
+    public SendMessage buildWelcomeMessage(Update update) {
+        var message = buildSendMessage(
+                update.getMessage().getChatId().toString(),
+                MessageUtils.getWelcomeMessage()
+        );
+
+        botButtonManager.addRequestContactButton(message);
+        return message;
+    }
+
+    private SendMessage buildSendMessage(String chatId, String text) {
+        return SendMessage.builder()
+                .chatId(chatId)
+                .text(text)
+                .parseMode(ParseMode.HTML)
+                .build();
+    }
+
+}
