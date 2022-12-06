678ed4703b9492343242fbdfb85f25a837a85739
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2022-12-06 16:48:55 +0500

    Исправлен контроллер

--- src/main/java/com/stegnin/jiraved/controller/JiraVedController.java
+++ src/main/java/com/stegnin/jiraved/controller/JiraVedController.java
@@ -1,19 +1,24 @@
 package com.stegnin.jiraved.controller;
 
+import com.stegnin.jiraved.config.JsonExtension;
 import com.stegnin.jiraved.config.property.AppProperty;
 import java.util.Objects;
 import javax.ws.rs.BadRequestException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.ExtensionMethod;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "api")
+@RestController
+@RequestMapping("api")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@ExtensionMethod(JsonExtension.class)
 @RequiredArgsConstructor
 @Slf4j
 public class JiraVedController {
@@ -22,15 +27,15 @@ public class JiraVedController {
 
     @PostMapping(path = "/{token}/jira")
     public String handleJiraRequest(@PathVariable String token, @RequestBody Object jiraObject) {
-        log.info("Handle Jira request");
+        log.info("Handle Jira request {}", jiraObject.toJson());
         checkToken(token);
-        log.info("Jira request {}", jiraObject);
         return "Success";
     }
 
     private void checkToken(String token) {
         if (!Objects.equals(token, appProperty.getToken())) {
-            throw new BadRequestException("Token invalid");
+            log.error("Invalid token");
+            throw new BadRequestException("Invalid token");
         }
     }
 
